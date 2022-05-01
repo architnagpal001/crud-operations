a706d310e33bff9ebaec0e48b7b91b045f5b08d0
Archit Nagpal <architnagpal312@gmail.com>
2022-05-01 13:13:20 +0530

    previous built

--- app.js
+++ app.js
@@ -26,18 +26,42 @@ app.get('/loan_predict', (req, res)=>{
 //     client.end;
 // })
 
+// app.get('/loan_predict/:id', (req, res)=>{
+//     client.query('Select * from loan_predict where id= ?',[req.params.id], (err, rows, fields)=>{
+//         if(!err){
+//             res.send(rows);
+//         }
+
+//     });
+//     client.end;
+//   });
 app.get('/loan_predict/:id', (req, res)=>{
-    client.query('Select * from loan_predict where id= ?',[req.params.id], (err, rows, fields)=>{
+    const id = req.params.id;
+    client.query(`Select * from loan_predict WHERE id = ${id}`, (err, result)=>{
         if(!err){
-            res.send(rows);
+            res.send(result.rows);
         }
 
     });
     client.end;
-
-app.post('/loan_predict', client.createUser)
-
-
-    // app.put('/users/:id', db.updateUser)
-    // app.delete('/users/:id', db.deleteUser)
-})
+});
+// app.put('/loan_predict/:id', (req, res)=>{
+//     const id = req.params.id;
+//     const data = req.body;
+//     client.query(`UPDATE loan_predict SET ? WHERE id = ${id}`, data = data.json, (err, result)=>{
+//         if(!err){
+//             res.send(result.rows);
+//         }
+//     });
+//     client.end;
+// });
+app.delete('/loan_predict/:id', (req, res)=>{
+    const id = req.params.id;
+    const data = req.body;
+    client.query(`DROP loan_predict SET ? WHERE id = ${id}`, data , (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+});
--- connection.js
+++ connection.js
@@ -8,9 +8,9 @@ const client = new Client({
     database: "postgres"
 })
 const getUserById = (req, res) => {
-    client.query('SELECT * FROM loan_predict WHERE id = ?', [req.params.Loan_ID], (err, rows, fields) => {
+    client.query('SELECT * FROM loan_predict WHERE id = ?', [req.params.id], (err, rows, fields) => {
       if (err) {
-        throw error
+        throw error;
       }
       res.status(200).json(rows)
     })
@@ -23,6 +23,17 @@ const getUserById = (req, res) => {
 //       response.status(200).json(results.rows)
 //     })
 //   }
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM loan_predict WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
 
 module.exports = client
 // module.exports = getUsers
