ac07e168b127e76447245332b5e61a831c2d38ba
Archit Nagpal <architnagpal312@gmail.com>
2022-05-01 15:23:28 +0530

    I think it is done

--- app.js
+++ app.js
@@ -1,67 +1,24 @@
-const client = require('./connection.js')
+const db = require('./connection.js')
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+//get all elements
+app.get('/loan_predict', db.getAllUsers);
+//getelement by id
+app.get('/loan_predict/:id', db.getUserById);
+//delete
+app.delete('/loan_predict/:id', db.deleteUser);
+app.post('/loan_predict', db.insertUser);
+
+app.post('/loan_predict/:id', db.updateUser);
 
 app.listen(3300, ()=>{
-    console.log("Sever is now listening at port 3300");
+    console.log("Server is now listening at port 3300");
 })
-
-client.connect();
-
-app.get('/loan_predict', (req, res)=>{
-    client.query(`Select * from loan_predict`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-})
-
-// app.get('/loan_prediction/Loan_ID', (req, res)=>{
-//     client.query(`Select * from loan_prediction ORDER BY Loan_ID DESC`, (err, result)=>{
-//         if(!err){
-//             res.send(result.rows);
-//         }
-//     });
-//     client.end;
-// })
-
-// app.get('/loan_predict/:id', (req, res)=>{
-//     client.query('Select * from loan_predict where id= ?',[req.params.id], (err, rows, fields)=>{
-//         if(!err){
-//             res.send(rows);
-//         }
-
-//     });
-//     client.end;
-//   });
-app.get('/loan_predict/:id', (req, res)=>{
-    const id = req.params.id;
-    client.query(`Select * from loan_predict WHERE id = ${id}`, (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-
-    });
-    client.end;
-});
-// app.put('/loan_predict/:id', (req, res)=>{
-//     const id = req.params.id;
-//     const data = req.body;
-//     client.query(`UPDATE loan_predict SET ? WHERE id = ${id}`, data = data.json, (err, result)=>{
-//         if(!err){
-//             res.send(result.rows);
-//         }
-//     });
-//     client.end;
-// });
-app.delete('/loan_predict/:id', (req, res)=>{
-    const id = req.params.id;
-    const data = req.body;
-    client.query(`DROP loan_predict SET ? WHERE id = ${id}`, data , (err, result)=>{
-        if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-});
--- connection.js
+++ connection.js
@@ -7,34 +7,73 @@ const client = new Client({
     password: "postgres",
     database: "postgres"
 })
+
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+//get all elements
+const getAllUsers = (req, res)=>{
+    client.query(`Select * from loan_predict`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+}
+
+//getusers by id
+
 const getUserById = (req, res) => {
-    client.query('SELECT * FROM loan_predict WHERE id = ?', [req.params.id], (err, rows, fields) => {
+    const id = req.params.id
+    client.query(`SELECT * FROM loan_predict WHERE id = ${id}`, (err, rows) => {
       if (err) {
         throw error;
       }
-      res.status(200).json(rows)
+      res.status(200).send(rows)
     })
   }
-// const getUsers = (request, response) => {
-//     client.query('SELECT * FROM loan_prediction ORDER BY Loan_ID ASC', (error, results) => {
-//       if (error) {
-//         throw error
-//       }
-//       response.status(200).json(results.rows)
-//     })
-//   }
+
 
 const deleteUser = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM loan_predict WHERE id = $1', [id], (error, results) => {
+  client.query(`DELETE FROM loan_predict WHERE id = ${id}`, (error, results) => {
     if (error) {
       throw error
     }
     response.status(200).send(`User deleted with ID: ${id}`)
   })
 }
+const insertUser = (request, response) => {
+  const { id , dependents } = request.body
+  client.query('INSERT INTO loan_predict (id, dependents) VALUES ($1, $2)', [id , dependents], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`User added with ID: ${id}`)
+    })
+}
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const {dependents, credit_history} = request.body
 
-module.exports = client
-// module.exports = getUsers
-
+  client.query(
+    `UPDATE loan_predict SET dependents = ${dependents}, credit_history = ${credit_history} WHERE id = ${id}`,
+   
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+module.exports = {
+    getAllUsers,
+    getUserById,
+    deleteUser,
+    insertUser,
+    updateUser
+}
